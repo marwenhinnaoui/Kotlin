0ed27bcc8c8170d2d7fe1b48b121c000ddadd2f0
ISETBZ-INFO <m>
2022-05-26 13:48:18 +0100

    INIT

---
+++ app/src/main/java/com/example/tpkotlin/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.tpkotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.tpkotlin.ui.theme.TpKotlinTheme
+data class Owner(val name: String, val bio: String, val image:Int)
+
+class Dog(
+    val id: Int,
+    val name: String,
+    val age: Double,
+    val gender: String,
+    val color: String,
+    val weight: Double,
+    val location: String,
+    val image: Int,
+    val about: String,
+    val owner:Owner
+)
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TpKotlinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+
+                ) {
+                    DogList(FakeDogDatabase.dogList)
+                }
+            }
+        }
+    }
+}
+@Composable
+fun DogCard(dog:Dog){
+
+    Surface(
+    ) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 10.dp, shape = RoundedCornerShape(30.dp), color = Color.White)
+            .background(Color.White),
+                verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = painterResource(id = dog.image,),
+            contentDescription = "Profile image",
+            modifier = Modifier
+                .size(150.dp)
+                .clip(shape = CircleShape)
+                .border(
+                    width = 20.dp,
+                    color = Color.White,
+                    shape = RoundedCornerShape(35.dp)
+                )
+        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+        ) {
+
+            Text(text = dog.name)
+            Text(text = "${dog.age}")
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(text = dog.location)
+                Text(text = dog.name)
+            }
+
+
+
+        }
+    }
+    }
+
+}
+
+
+@Composable
+fun DogList(dogs: List<Dog>) {
+    Scaffold(
+        topBar = {
+            TopAppBar() {
+                
+            }
+        }
+    ){
+
+    LazyColumn(
+    ) {
+        items(dogs) { dog ->
+            DogCard(dog)
+        }
+    }
+    }
+
+}
+
